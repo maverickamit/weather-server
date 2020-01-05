892873c0321aee94b52099c0794dab6099c23626
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-05 18:49:57 +0530

    created four endpoints

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get("", (req, res) => {
   res.send("Home page");
 });
 
+app.get("/help", (req, res) => {
+  res.send("Help page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("Your weather");
+});
+
 app.listen(3000, () => {
   console.log("server is running in port 3000");
 });
