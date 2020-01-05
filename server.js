0df5677cd4049004ee7df9ac9b9e26002018ca31
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-05 19:37:54 +0530

    implemented handlebars viewengine and deleted static html files

--- server.js
+++ server.js
@@ -5,19 +5,21 @@ const app = express();
 
 const publicDirectory = path.join(__dirname, "/public");
 
+app.set("view engine", "hbs");
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/help", (req, res) => {
+  res.render("help");
+});
+
 app.use(express.static(publicDirectory));
-
-// app.get("", (req, res) => {
-//   res.send("Home page");
-// });
-
-// app.get("/help", (req, res) => {
-//   res.send("Help page");
-// });
-
-// app.get("/about", (req, res) => {
-//   res.send("About page");
-// });
 
 app.get("/weather", (req, res) => {
   res.send("Your weather");
