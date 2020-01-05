4f6a65dc5ec93c44fa7ab9f9a67eb79ce3cff07d
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-06 00:55:51 +0530

    setting for handlebar

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const express = require("express");
 
 const app = express();
 
+// Define paths for express configs
 const publicDirectory = path.join(__dirname, "/public");
+const viewsPath = path.join(__dirname, "/template");
 
+//Setup handlebars and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
 app.get("", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Weather Forecaster",
+    name: "Amit Ghosh"
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -16,9 +23,12 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/help", (req, res) => {
-  res.render("help");
+  res.render("help", {
+    helpText: "This is some helpful text"
+  });
 });
 
+//Setup static directory for serve
 app.use(express.static(publicDirectory));
 
 app.get("/weather", (req, res) => {
