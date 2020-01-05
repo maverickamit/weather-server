05571716e234f6f26e3fc47a98125604e518fe5f
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-05 19:10:25 +0530

    using app.use instead of app.get

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Home page");
-});
+const publicDirectory = path.join(__dirname, "/public");
 
-app.get("/help", (req, res) => {
-  res.send("Help page");
-});
+app.use(express.static(publicDirectory));
 
-app.get("/about", (req, res) => {
-  res.send("About page");
-});
+// app.get("", (req, res) => {
+//   res.send("Home page");
+// });
+
+// app.get("/help", (req, res) => {
+//   res.send("Help page");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("About page");
+// });
 
 app.get("/weather", (req, res) => {
   res.send("Your weather");
