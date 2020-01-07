e7056ce9bb0959cfacf31d4fba30dabd5afa8d7b
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-07 17:04:40 +0530

    displaying the json response on website

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,27 @@ console.log("client side javascript file is loaded!");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#one");
+const messageTwo = document.querySelector("#two");
+
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
-  if(!search.value){
-      
+  if (!search.value) {
   }
-  
+
   const location = search.value;
   const fetchURL = "http://localhost:3000/weather?address=" + location;
+  messageOne.textContent = "";
+
+  messageTwo.textContent = "Loading ...";
 
   fetch(fetchURL).then(res => {
     res.json().then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageTwo.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
