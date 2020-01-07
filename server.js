d6a0344fa6cebbdc90ddeb589529ea6ee76ecec2
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-07 11:25:27 +0530

    styling frontend

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for express configs
 const publicDirectory = path.join(__dirname, "/public");
-const viewsPath = path.join(__dirname, "/template");
+const viewsPath = path.join(__dirname, "/template/views");
+const partialsPath = path.join(__dirname, "/template/partials");
 
 //Setup handlebars and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+
+//setup partials
+hbs.registerPartials(partialsPath);
 
 app.get("", (req, res) => {
   res.render("index", {
@@ -19,12 +24,17 @@ app.get("", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    title: "About",
+    name: "Amit Ghosh"
+  });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    helpText: "This is some helpful text"
+    title: "Help",
+    helpText: "This is some helpful text",
+    name: "Amit Ghosh"
   });
 });
 
@@ -33,6 +43,22 @@ app.use(express.static(publicDirectory));
 
 app.get("/weather", (req, res) => {
   res.send("Your weather");
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("error", {
+    title: 404,
+    errorMessage: "Help article not found.",
+    name: "Amit Ghosh"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("error", {
+    title: 404,
+    errorMessage: "Page not found.",
+    name: "Amit Ghosh"
+  });
 });
 
 app.listen(3000, () => {
